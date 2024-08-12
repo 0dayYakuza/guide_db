b91ecf588d49312b95835c4096e04f5e4f7fa0cb
Ruski <ru5ki0day@yandex.ru>
2024-08-12 22:55:09 +0300

    First push

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>DarkBot</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/darkbot-reloaded',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/darkbot',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template'
 }
 
 export default config
